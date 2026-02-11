41479c7ee74bb37aa90bfe3c2282317e263a55aa
ardhiyudha <ardhi.yudha.id@gmail.com>
2026-02-11 18:22:04 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+const links = document.querySelectorAll(".nav-links a");
+const currentPage = window.location.pathname.split("/").pop();
+
+links.forEach(link => {
+  if (link.getAttribute("href") === currentPage) {
+    link.classList.add("active");
+  }
+});
