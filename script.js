e52a47a68eea3cbb5fa5d2be5e7ec81a6f5effda
ardhiyudha <ardhi.yudha.id@gmail.com>
2026-02-16 15:15:12 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-const links = document.querySelectorAll(".nav-links a");
-const currentPage = window.location.pathname.split("/").pop();
+// Smooth Scroll
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href"))
+      .scrollIntoView({ behavior: "smooth" });
+  });
+});
 
-links.forEach(link => {
-  if (link.getAttribute("href") === currentPage) {
-    link.classList.add("active");
+// Navbar shadow on scroll
+const navbar = document.querySelector(".navbar");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 20) {
+    navbar.style.boxShadow = "0 4px 12px rgba(0,0,0,0.05)";
+  } else {
+    navbar.style.boxShadow = "none";
   }
 });
